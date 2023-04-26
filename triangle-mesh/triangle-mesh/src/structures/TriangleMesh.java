44784bd81d89aff9326e05c9dd6fe447d1ede570
Davide Giambona <86895372+davidegiambona@users.noreply.github.com>
2023-04-26 11:55:02 +0200

    Add files via upload

--- triangle-mesh/triangle-mesh/src/structures/TriangleMesh.java
+++ triangle-mesh/triangle-mesh/src/structures/TriangleMesh.java
@@ -1,1 +1,31 @@
+package structures;
 
+import java.util.List;
+import java.util.ArrayList;
+import shapes.Triangle2D;
+import shapesfactory.TriangleFactory;
+
+public class TriangleMesh {
+	List<Triangle2D> mesh;
+
+	public TriangleMesh( ) {
+		mesh = new ArrayList<Triangle2D>();
+		mesh.add(TriangleFactory.build());
+	}	
+
+	public void add() {
+		mesh.add(TriangleFactory.build(getLastTriangle()));
+	}
+	
+	private Triangle2D getLastTriangle() {
+		return mesh.get(mesh.size()-1);
+	}
+	
+	public int getSize() {
+		return mesh.size();
+	}
+	
+	public Triangle2D get(int index) {
+		return mesh.get(index);
+	}
+}
